c14dca5fa9b043435c7bdf867b929863bd2a4054
Dielson Sales <dielson@ilhasoft.com.br>
2019-05-30 19:15:53 -0300

    Squashed commit of the following:
    commit c979a271e092148944f3bf49072d4c552ef27787
    Author: Dielson Sales <dielson@ilhasoft.com.br>
    Date:   Thu May 30 19:15:06 2019 -0300
    
        Add more cython code
    
    commit b40a69d872ceda41bf25d02777b47b5b86e1faab
    Author: Dielson Sales <dielson@ilhasoft.com.br>
    Date:   Thu May 30 17:36:52 2019 -0300
    
        Update native code
    
    commit 43367b40e0bb7bb0c6bb215f280c246fdb04d369
    Author: Dielson Sales <dielson@ilhasoft.com.br>
    Date:   Thu May 30 16:54:22 2019 -0300
    
        Update Cython code
    
    commit fcb027ed96fa06cf1d6c7cbf13685344c2c63b30
    Author: Dielson Sales <dielson@ilhasoft.com.br>
    Date:   Thu May 30 16:44:13 2019 -0300
    
        Create main files
    
    commit 13303ea3b1dbdea7bfa747b44b9b7ea950422ba0
    Author: Dielson Sales <dielson@ilhasoft.com.br>
    Date:   Thu May 30 16:25:41 2019 -0300
    
        Add .gitignore to Cython

---
+++ Cython/main.py
@@ -0,0 +1,9 @@
+import native_module
+
+
+def main():
+	native_module.read_file("input.txt")
+
+
+if __name__ == '__main__':
+	main()
---
+++ Cython/native_module.pyx
@@ -0,0 +1,26 @@
+from random import randint
+from libc.stdio cimport FILE, fopen, fclose, fprintf, fgets, printf
+
+
+cpdef c_function():
+	cdef int sum = 0
+	for i in range(100):
+		sum += randint(0, 100)
+	return sum
+
+
+cpdef write_to_file():
+	cdef FILE* p
+	p = fopen("test.txt", "w")
+	fprintf(p, "✊")
+	fclose(p)
+
+
+cpdef read_file(input_path):
+	cdef FILE* p
+	cdef char s[100]
+	p = fopen(input_path.encode(), "r")
+	while fgets(s, 100, p) != NULL:
+		print(str(s, 'utf-8'))
+		#printf("%s", s)
+	fclose(p)
